1ed1e758adca34e0000f3bef03846bdb9f6a67fe
Vijay Shankar <vijayoffcl@gmail.com>
2023-08-08 21:42:15 +0530

    Update About.js

--- src/Component/About.js
+++ src/Component/About.js
@@ -16,7 +16,7 @@ export default function About() {
       remaining essentially unchanged. It was popularised in the 1960s
       with the release of Letraset sheets containing Lorem Ipsum passages,
       and more recently with desktop publishing software like Aldus
-      PageMaker including versions of Lorem Ipsum."
+      PageMaker including versions of Lorem Ipsum My-First-React-Webpage."
         imgsrc={homeimg}
         btn="Know more"
       />
